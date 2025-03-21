86cfb33818376eaa968b431f990dbc1b9fca9e76
kunalBari5557 <kunalaage21@gmail.com>
2025-03-21 16:53:55 +0530

    fix drawer close  issue

--- components/layout/navbar/nav-item.js
+++ components/layout/navbar/nav-item.js
@@ -4,24 +4,33 @@ import { useState } from "react";
 import DropdownMenu from "./dropdown-menu";
 import MegaMenu from "./mega-menu";
 
-export default function NavItem({ navItemText, menuItems, megaMenu = false }) {
-	const [isOpenDropDown, setIsOpenDropDown] = useState(false);
-	const handleClick = (e) => {
-		setIsOpenDropDown(!isOpenDropDown);
-	};
+export default function NavItem({
+  navItemText,
+  handleCloseMobileMenu,
+  menuItems,
+  megaMenu = false,
+}) {
+  const [isOpenDropDown, setIsOpenDropDown] = useState(false);
+  const handleClick = (e) => {
+    setIsOpenDropDown(!isOpenDropDown);
+  };
 
-	return (
-		<li className={`nav-item ${menuItems ? "nav-item-has-children" : ""}`}>
-			<a href="#" className="nav-link-item drop-trigger" onClick={handleClick}>
-				{navItemText}
-				<FontAwesomeIcon icon={faAngleDown} />
-			</a>
+  return (
+    <li className={`nav-item ${menuItems ? "nav-item-has-children" : ""}`}>
+      <a href="#" className="nav-link-item drop-trigger" onClick={handleClick}>
+        {navItemText}
+        <FontAwesomeIcon icon={faAngleDown} />
+      </a>
 
-			{!megaMenu ? (
-				<DropdownMenu isOpenDropDown={isOpenDropDown} menuItems={menuItems} />
-			) : (
-				<MegaMenu menuItems={menuItems} />
-			)}
-		</li>
-	);
+      {!megaMenu ? (
+        <DropdownMenu
+          isOpenDropDown={isOpenDropDown}
+          handleCloseMobileMenu={handleCloseMobileMenu}
+          menuItems={menuItems}
+        />
+      ) : (
+        <MegaMenu menuItems={menuItems} />
+      )}
+    </li>
+  );
 }
