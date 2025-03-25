ec0e2053622ff64aca32f693cc6229f91b1bbabc
Snehal-seenit <snehal@seenit.co>
2025-03-25 13:19:21 +0530

    icons changes added

--- components/common/video/video-section-two.js
+++ components/common/video/video-section-two.js
@@ -34,28 +34,28 @@ export default function VideoSectionTwo() {
 							<div className="fugu-icon-list">
 								<ul>
 									<li>
-										{/* <img src="/images/svg/check1.svg" alt="" /> */}
-										<p className="mb-0">💰 Spot Trading</p>
+									<img src="/images/svg/icons_1.png" alt="" />
+										<p className="mb-0">Spot Trading</p>
 										<small className="fw-normal"> Buy and sell cryptocurrencies with real-time high liquidity, allowing you to capitalize on market movements instantly and efficiently.</small>
 									</li>
 									<li>
-										{/* <img src="/images/svg/check1.svg" alt="" /> */}
-										<p className="mb-0">📊 Margin Trading</p>
+									<img src="/images/svg/icons_1.png" alt="" />
+										<p className="mb-0">Margin Trading</p>
 										<small className="fw-normal">Boost your trading power with our leveraged trading options. Maximize your potential returns by trading with more capital than you initially possess, while managing the risks wisely.</small>
 									</li>
 									<li>
-										{/* <img src="/images/svg/check1.svg" alt="" /> */}
-										<p className="mb-0">📈 Copy Trading</p>
+									<img src="/images/svg/icons_1.png" alt="" />
+										<p className="mb-0">Copy Trading</p>
 										<small className="fw-normal">Mirror the strategies of top-performing traders effortlessly. With our copy trading feature, you can follow and replicate the trades of successful investors, making it easier to navigate the market.</small>
 									</li>
 									<li>
-										{/* <img src="/images/svg/check1.svg" alt="" /> */}
-										<p className="mb-0">📈 Futures Trading</p>
+									<img src="/images/svg/icons_1.png" alt="" />
+										<p className="mb-0">Futures Trading</p>
 										<small className="fw-normal">Engage in futures trading to speculate on the future price of cryptocurrencies. With our futures contracts, you can profit from both rising and falling markets, providing flexibility and more ways to trade.</small>
 									</li>
 									<li>
-										{/* <img src="/images/svg/check1.svg" alt="" /> */}
-										<p className="mb-0">⚡ Crypto Swap</p>
+									<img src="/images/svg/icons_1.png" alt="" />
+										<p className="mb-0">Crypto Swap</p>
 										<small className="fw-normal">Convert assets seamlessly with our instant crypto swap feature. Swap one cryptocurrency for another without delays, allowing you to take advantage of market opportunities as they arise.</small>
 									</li>
 								</ul>
