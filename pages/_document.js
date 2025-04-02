8999c399f2c17b05859202192887521971036744
Snehal-seenit <snehal@seenit.co>
2025-04-02 17:59:07 +0530

    twitter link updated and chatbot added

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,9 @@ export default function Document() {
 					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Public+Sans:wght@500;600;700;800&display=swap"
 					rel="stylesheet"
 				/>
+				
+			<script defer="defer" type="text/javascript" src="https://www.humb.support/im_livechat/loader/2"></script>
+            <script defer="defer" type="text/javascript" src="https://www.humb.support/im_livechat/assets_embed.js"></script>
 			</Head>
 			<body>
 				<Main />
