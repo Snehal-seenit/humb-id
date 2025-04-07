86a2566c34e185f9ab1f009264f91057c2bb0057
Snehal-seenit <snehal@seenit.co>
2025-04-07 17:45:28 +0530

    reward page developed

---
+++ pages/fee.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function CommingSoon() {
+	return (
+		<div className="section bg-black">
+			<div className="container">
+				<div className="fugu-error-content wow fadeInUpX">
+					
+					<h2>We are comming soon</h2>
+					
+					
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export async function getStaticProps() {
+	return { props: { header: "one", footer: "one" } };
+}
