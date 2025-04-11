d6bf9ffda968e2947f358a887373b92c8a0c3e8f
shaanicha <chaudharishraddha126@gmail.com>
2025-04-11 12:25:43 +0530

    chore: update ESLint config to support Jest and clean rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
-  extends: ['next', 'next/core-web-vitals'],
+  extends: ['next/core-web-vitals'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true, // ✅ Add Jest environment support
+  },
   rules: {
     'react/no-unescaped-entities': 'warn',
     '@next/next/no-img-element': 'warn',
     'no-unused-vars': 'warn',
-    'react/prop-types': 'warn'
+    'react/prop-types': 'warn',
   },
 };
