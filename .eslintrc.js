574c004b9eb2b0e71cab563457081d500fc662dc
shaanicha <chaudharishraddha126@gmail.com>
2025-04-11 11:54:05 +0530

    � update ESLint rules to show warnings instead of errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ['next', 'next/core-web-vitals'],
   rules: {
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-img-element': 'off',
+    'react/no-unescaped-entities': 'warn',
+    '@next/next/no-img-element': 'warn',
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'warn'
   },
 };
